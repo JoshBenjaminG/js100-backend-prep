191fc17a2eba50da020fcacc76b5b654d1b3e51d
JoshuaEgage <93669172+JoshuaEgage@users.noreply.github.com>
2025-07-07 17:46:40 -0500

    last changes

---
+++ arrays.js
@@ -0,0 +1,28 @@
+let array1 = [1, 2, undefined, 4];
+
+let array2 = [1];
+array2.length = 5;
+
+let array3 = [];
+array3[-1] = [1];
+
+let array4 = [1, 2, 3, 4, 5];
+array4.length = 3;
+
+let array5 = [];
+array5[100] = 3;
+
+// 4, 5, 0, 3, 101
+
+let myArray = [
+  [1, 3, 6, 11],
+  [4, 2, 4],
+  [9, 17, 16, 0],
+];
+
+for (let i = 0; i < myArray.length; i++) {
+  for (let k = 0; k < myArray[i].length; k++) {
+    console.log(myArray[i][k]);
+  }
+}
+
